d7c13d588f09e2e93c9409641b6fa1427303fbf4
Skandeerkefi <skanderkefi88@gmail.com>
2025-10-06 01:58:04 +0100

    WIP: Save progress before merging into master

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const leaderboardController = require("../controllers/leaderboardController.js");
-const fetch = require("node-fetch");
+
 // Get leaderboard with optional date range
 router.get("/", leaderboardController.getLeaderboard);
-
 
 // Get leaderboard for a specific date range
 router.get("/:startDate/:endDate", leaderboardController.getLeaderboardByDate);
